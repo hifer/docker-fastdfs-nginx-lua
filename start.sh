a9e304c9b7c1f60a1c4d8cc672ab843c530759c4
hifer <lvhifer@163.com>
2020-08-08 13:53:50 +0800

    适配官网美化配置文件等号前后加空格问题

--- start.sh
+++ start.sh
@@ -19,16 +19,16 @@ ln -s /data/fastdfs/storage/data/ /data/fastdfs/storage/data/M00;
 sed -i "s/listen .*$/listen $NGINX_PORT;/g" /usr/local/nginx/conf/nginx.conf; 
 
 # fdfs配置
-sed -i "s/http.server_port=.*$/http.server_port=$NGINX_PORT/g" /etc/fdfs/storage.conf; 
+sed -i "s/http.server_port.*$/http.server_port = $NGINX_PORT/g" /etc/fdfs/storage.conf; 
 if [ "$HOST_IP" = "" ]; then 
     HOST_IP=$(ifconfig $NETWORK_CARD | grep "inet" | grep -v "inet6" | awk '{print $2}' | awk -F: '{print $2}')
 fi 
 
-sed -i "s+^tracker_server=.*$+tracker_server=$HOST_IP:$FDFS_PORT+g" /etc/fdfs/storage.conf;
-sed -i "s+^tracker_server=.*$+tracker_server=$HOST_IP:$FDFS_PORT+g" /etc/fdfs/client.conf;
-sed -i "s+^tracker_server=.*$+tracker_server=$HOST_IP:$FDFS_PORT+g" /etc/fdfs/mod_fastdfs.conf;
+sed -i "s+^tracker_server.*$+tracker_server = $HOST_IP:$FDFS_PORT+g" /etc/fdfs/storage.conf;
+sed -i "s+^tracker_server.*$+tracker_server = $HOST_IP:$FDFS_PORT+g" /etc/fdfs/client.conf;
+sed -i "s+^tracker_server.*$+tracker_server = $HOST_IP:$FDFS_PORT+g" /etc/fdfs/mod_fastdfs.conf;
 #sed -i "s+^group_count =.*$+group_count = $GROUP_COUNT+g" /etc/fdfs/mod_fastdfs.conf;
-sed -i "s+^store_path0.*$+store_path0=$STORAGE_PATH+g" /etc/fdfs/mod_fastdfs.conf;
+sed -i "s+^store_path0.*$+store_path0 = $STORAGE_PATH+g" /etc/fdfs/mod_fastdfs.conf;
 sed -i "s+^url_have_group_name =.*$+url_have_group_name = true+g" /etc/fdfs/mod_fastdfs.conf;
 
 # 启动
